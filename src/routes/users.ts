e80488593cc175b22561af7b6e358387af8e0a16
matsushinDB11 <matsushin35@gmail.com>
2021-10-13 02:04:49 +0900

    install swagger-jsdoc

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,10 +2,32 @@ import express from 'express';
 
 const router = express.Router();
 
+/**
+ *  @swagger
+ *  /users:
+ *    title: ユーザー
+ *    get:
+ *      description: Get UserID & Name
+ *      response:
+ *       200:
+ *        description: Success
+ *        content: application/json
+ */
 // GETリクエスト
 router.get('/', (req: express.Request, res: express.Response) => {
   res.status(200).json({ userId: "U001", userName: "Yamada Taro" });
 });
+
+/**
+ * @swagger
+ * post:
+ *  description: Add User
+ *  response:
+ *   204:
+ *    description: Success
+ *   400:
+ *    description: Bad request
+ */
 
 // POSTリクエスト
 router.post('/', (req: express.Request, res: express.Response) => {
---
+++ swaggerDefinition.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    info: {
+        // API informations (required)
+        openapi: '3.0.0',
+        title: 'SQL-Learning-MK3 test', // Title (required)
+        version: '1.0.0', // Version (required)
+        description: 'Testing for SQL-Learning-MK3_backend', // Description (optional)
+    },
+    // host: `localhost:${PORT}`, // Host (optional)
+    // basePath: '/', // Base path (optional)
+    apis: ['./dist/index.js']
+};
