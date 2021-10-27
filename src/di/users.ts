4406db835631a1f0de87d421257b7dc6cae01315
matsushinDB11 <matsushin35@gmail.com>
2021-10-27 18:57:45 +0900

    create di for users

--- src/di/users.ts
+++ src/di/users.ts
@@ -1,11 +1,12 @@
 import {Prisma} from "../infra/repository/prisma";
 import {usersController} from "../controller/users";
 import {usersUsecase} from "../usecase/users/endpoint";
+import {usersInfra} from "../infra/repository/users";
 
 export function CreateUsersController():usersController {
     const i = new usersUsecase(
-        // Infra
-        // Prisma
+        new usersInfra(),
+        new Prisma()
     )
     return new usersController(i);
 }
