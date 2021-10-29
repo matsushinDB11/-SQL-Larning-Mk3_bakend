c6bace4816e30dd2ef2b4fab2211923ed52cc514
matsushinDB11 <matsushin35@gmail.com>
2021-10-29 20:22:58 +0900

    rm prisma domain & infra

--- src/infra/repository/prisma.ts
+++
@@ -1,8 +0,0 @@
-import {PrismaInterface} from "../../domain/prisma";
-import {PrismaClient} from '@prisma/client';
-
-export class Prisma implements PrismaInterface{
-    constructor() {
-        return new PrismaClient()
-    }
-}
