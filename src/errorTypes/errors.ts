4ff402502274ad5d233b0d21b9013fd4b0a107bb
matsushinDB11 <matsushin35@gmail.com>
2021-10-29 23:10:10 +0900

    add methods to resourceNotFoundError class

--- src/errorTypes/errors.ts
+++ src/errorTypes/errors.ts
@@ -1,1 +1,10 @@
-class notfoundError extends Error {}
+class resourceNotFoundError extends Error {
+    private readonly resource: string;
+    constructor(resource: string) {
+        super();
+        this.resource = resource;
+    }
+    CatError = () => {
+        return this.resource + "not found"
+    }
+}
