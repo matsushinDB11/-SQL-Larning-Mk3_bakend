fe1723bd9ae40624a1620f1c10e863b1004ca47b
matsushinDB11 <matsushin35@gmail.com>
2021-10-30 00:08:27 +0900

    fix error handling

--- src/usecase/users/logic.ts
+++ src/usecase/users/logic.ts
@@ -1,17 +1,15 @@
 import {Repository} from "../../domain/users";
 import {PrismaClient, User} from "@prisma/client";
-import {convertGetOutput, user} from "./output";
 
 export async function getList(prisma: PrismaClient, userRepo: Repository):Promise<User[]> {
     return await userRepo.GetList(prisma);
 }
 
-export async function get(prisma: PrismaClient, userRepo: Repository, input: GetInput):Promise<Result<user, Error>> {
+export async function get(prisma: PrismaClient, userRepo: Repository, input: GetInput):Promise<Result<User, Error>> {
     const data = await userRepo.Get(prisma, input.userID);
     if (data.isFailure()) {
         return new Failure(data.value);
     } else {
-        const convertedData = convertGetOutput(data.value);
-        return new Success(convertedData);
+        return new Success(data.value);
     }
 }
