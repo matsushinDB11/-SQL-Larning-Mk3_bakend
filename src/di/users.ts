0c954c9637e2e64fdfa736b6f9195bb8df7a6455
matsushinDB11 <matsushin35@gmail.com>
2021-11-07 09:11:02 +0900

    switch PrismaClient to PrismaInfra

--- src/di/users.ts
+++ src/di/users.ts
@@ -1,12 +1,12 @@
-import {PrismaClient} from "@prisma/client";
 import {usersController} from "../controller/users";
 import {usersUsecase} from "../usecase/users/endpoint";
 import {usersInfra} from "../infra/repository/users";
+import {PrismaInfra} from "../infra/repository/PrismaInfra";
 
 export function CreateUsersController():usersController {
     const i = new usersUsecase(
         new usersInfra(),
-        new PrismaClient()
+        new PrismaInfra()
     )
     return new usersController(i);
 }
