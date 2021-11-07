f9960b0e9f7f1b5c4b34393506a5b4c875028e53
matsushinDB11 <matsushin35@gmail.com>
2021-11-07 14:02:13 +0900

    create httpStatusCodes class

---
+++ src/domain/httpStatusCodes.ts
@@ -0,0 +1,20 @@
+export default class HttpStatusCodes{
+    // 順次追加する
+    private readonly OK = 200;
+    private readonly Created = 201;
+    private readonly NoContent = 204;
+    private readonly BadRequest = 400;
+    private readonly Unauthorized = 401;
+    private readonly Forbidden = 403;
+    private readonly NotFound = 404;
+    private readonly InternalServerError = 500;
+    StatusOK():number {return this.OK}
+    StatusCreated():number {return this.Created}
+    StatusNoContent(): number {return this.NoContent}
+    StatusBadRequest(): number {return this.BadRequest}
+    StatusUnauthorized(): number {return this.Unauthorized}
+    StatusStatusForbidden(): number {return this.Forbidden}
+    StatusNotFound(): number {return this.NotFound}
+    StatusStatusInternalServerError(): number {return this.InternalServerError}
+}
+
