0e82e05be3cf6ddef900a10a49426a8ff53cc015
matsushinDB11 <matsushin35@gmail.com>
2021-11-10 16:23:53 +0900

    add user add route & controller

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -1,8 +1,9 @@
 import {Interactor} from "../usecase/users/endpoint"
 import express from "express";
-import {GetInput} from "../usecase/users/input";
+import {AddInput, GetInput} from "../usecase/users/input";
 import HttpStatusCodes from "../domain/httpStatusCodes";
 import ErrorToHttpStatus from "./errorToHttpStatus";
+import {Failure, Success} from "../errorTypes/resultType";
 const http = new HttpStatusCodes();
 
 export class usersController {
@@ -24,6 +25,20 @@ export class usersController {
             res.status(ErrorToHttpStatus(data.value)).json(data.value);
         } else {
             res.status(http.StatusOK()).json(data.value);
+        }
+    }
+    async Add(req: express.Request, res: express.Response) {
+        const name = req.body.name;
+        const email = req.body.email;
+        const input: AddInput = {
+            name: name,
+            email: email
+        }
+        const re = await this.interactor.Add(input)
+        if (re.isFailure()) {
+            res.status(ErrorToHttpStatus(re.value)).json(re.value);
+        } else {
+            res.status(http.StatusNoContent());
         }
     }
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -55,7 +55,7 @@ export default function usersRouter(s:Service):express.Router {
    */
   // POSTリクエスト
   router.post('/', (req: express.Request, res: express.Response) => {
-    res.status(200).json({ message: "登録しました" });
+    s.users.Add(req, res);
   });
   return router;
 }
