788935b0eb317fe88d7abbd98a548615e5e39756
matsushinDB11 <matsushin35@gmail.com>
2021-11-16 20:50:24 +0900

    rm employee di

--- src/di/di.ts
+++ src/di/di.ts
@@ -1,19 +1,14 @@
-import { employees as employeesController } from "../controller/employees";
-import { CreateEmployeesController } from "./employees";
 import { usersController } from "../controller/users";
 import { CreateUsersController } from "./users";
 
 export type Service = {
-    employees: employeesController;
     users: usersController;
 };
 
 export class NewService implements Service {
-    employees!: employeesController;
     users!: usersController;
     constructor() {
         return {
-            employees: CreateEmployeesController(),
             users: CreateUsersController(),
         };
     }
