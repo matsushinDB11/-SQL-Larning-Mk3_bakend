068e65cee9879e62d5ce730e77c1eb87f27d90bc
matsushinDB11 <matsushin35@gmail.com>
2021-11-16 20:52:46 +0900

    rm employee usecase

--- src/usecase/employees/logic.ts
+++
@@ -1,9 +0,0 @@
-import { employee, Repository } from "../../domain/employees/repository";
-import { PrismaClient } from "@prisma/client";
-
-export function getList(
-    prisma: PrismaClient,
-    employeeRepo: Repository
-): employee[] {
-    return employeeRepo.GetList(prisma);
-}
