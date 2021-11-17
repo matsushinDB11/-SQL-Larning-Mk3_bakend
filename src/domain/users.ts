ec9ce3f0d41f37c17e1ba53504f6f395997a1c03
matsushinDB11 <matsushin35@gmail.com>
2021-11-17 17:22:13 +0900

    add isAdmin arg for user add & update

--- src/domain/users.ts
+++ src/domain/users.ts
@@ -4,11 +4,16 @@ import { Result } from "../errorTypes/resultType";
 export interface Repository {
     GetList(dbClient: DBClient): Promise<user[]>;
     Get(dbClient: DBClient, userID: number): Promise<Result<user, Error>>;
-    Add(dbClient: DBClient, email: string): Promise<Result<void, Error>>;
+    Add(
+        dbClient: DBClient,
+        email: string,
+        isAdmin: boolean
+    ): Promise<Result<void, Error>>;
     Update(
         dbClient: DBClient,
         userID: number,
-        email: string
+        email: string,
+        isAdmin: boolean | undefined
     ): Promise<Result<void, Error>>;
     Delete(dbClient: DBClient, userID: number): Promise<Result<void, Error>>;
 }
