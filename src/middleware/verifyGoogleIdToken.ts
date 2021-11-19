50d8ad901245c30d8c18d07adadd9d3e4a5dfcdc
matsushinDB11 <matsushin35@gmail.com>
2021-11-19 18:50:29 +0900

    fix return type

--- src/middleware/verifyGoogleIdToken.ts
+++ src/middleware/verifyGoogleIdToken.ts
@@ -1,29 +1,20 @@
 import { OAuth2Client } from "google-auth-library";
 import keys = require("../GoogleAuth/apps.googleusercontent.com.json");
-import { Failure, Result, Success } from "../errorHelper/resultType";
-import { InternalServerError } from "../errorHelper/errors";
 
-export const verifyIdToken = async (
-    idToken: string
-): Promise<Result<verifyOutPut, Error>> => {
-    try {
-        const oAuth2Client = new OAuth2Client(
-            keys.web.client_id,
-            keys.web.client_secret
-            //keys.web.redirect_uris[0]
-        );
-        const ticket = await oAuth2Client.verifyIdToken({
-            idToken: idToken,
-            audience: keys.web.client_id,
-        });
-        const res: verifyOutPut = {
-            result: true,
-            userID: ticket.getUserId(),
-        };
-        return new Success(res);
-    } catch (e) {
-        return new Failure(new InternalServerError("verify Google Id_token"));
-    }
+export const verifyIdToken = async (idToken: string): Promise<verifyOutPut> => {
+    const oAuth2Client = new OAuth2Client(
+        keys.web.client_id,
+        keys.web.client_secret
+        //keys.web.redirect_uris[0]
+    );
+    const ticket = await oAuth2Client.verifyIdToken({
+        idToken: idToken,
+        audience: keys.web.client_id,
+    });
+    return {
+        result: true,
+        userID: ticket.getUserId(),
+    };
 };
 
 export type verifyOutPut = {
