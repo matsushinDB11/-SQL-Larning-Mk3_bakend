35e145751616be2fbc62b4f46a6f3e1f36066c85
matsushinDB11 <matsushin35@gmail.com>
2021-11-20 08:09:25 +0900

    create login controller

---
+++ src/controller/login.ts
@@ -0,0 +1,29 @@
+import { Interactor } from "../usecase/login/endpoint";
+import { Request, Response } from "express";
+import { LoginInput } from "../usecase/login/input";
+import ErrorToHttpStatus from "./errorToHttpStatus";
+import HttpStatusCodes from "../domain/httpStatusCodes";
+
+const http = new HttpStatusCodes();
+
+export class loginController {
+    protected interacotr: Interactor;
+    constructor(interactor: Interactor) {
+        this.interacotr = interactor;
+    }
+    Login(req: Request, res: Response) {
+        const id_token: string = req.body.id_token;
+        const input: LoginInput = {
+            googleIdToken: id_token,
+        };
+        this.interacotr.Login(input).then((data) => {
+            if (data.isFailure()) {
+                const errorStatus = ErrorToHttpStatus(data.value);
+                res.status(errorStatus).json(data.value);
+            } else {
+                const jwt = data.value.token;
+                res.status(http.StatusOK()).json({ jwt });
+            }
+        });
+    }
+}
