77e341de614a7e90c9d47350e76ca7afac6b19c3
matsushinDB11 <matsushin35@gmail.com>
2021-12-06 02:02:33 +0900

    create quesiton infra

---
+++ src/infra/repository/questions.ts
@@ -0,0 +1,57 @@
+import { question, Repository } from "../../domain/questions";
+import { Failure, Result, Success } from "../../errorHelper/resultType";
+import PrismaInfra from "./PrismaInfra";
+import { Question } from "@prisma/client";
+import {
+    DBInternalError,
+    resourceNotFoundError,
+} from "../../errorHelper/errors";
+
+class QuestionsInfra implements Repository {
+    GetList = async (
+        dbClient: PrismaInfra
+    ): Promise<Result<question[], Error>> => {
+        try {
+            const rowData: Question[] = await dbClient
+                .ConnectDB()
+                .question.findMany();
+            const resData: question[] = [];
+            rowData.forEach((data) => {
+                resData.push({
+                    ID: data.id,
+                    title: data.title,
+                    classID: data.classId,
+                });
+            });
+            return new Success(resData);
+        } catch (e) {
+            return new Failure(new DBInternalError("Get questions list"));
+        }
+    };
+    Get = async (
+        dbClient: PrismaInfra,
+        questionID: number
+    ): Promise<Result<question, Error>> => {
+        try {
+            const rowData = await dbClient.ConnectDB().question.findUnique({
+                where: {
+                    id: questionID,
+                },
+            });
+            if (rowData == null) {
+                return new Failure(
+                    new resourceNotFoundError("memoID: " + String(questionID))
+                );
+            } else {
+                const resData: question = {
+                    ID: rowData.id,
+                    title: rowData.title,
+                    classID: rowData.classId,
+                };
+                return new Success(resData);
+            }
+        } catch (e) {
+            return new Failure(new DBInternalError("Get question"));
+        }
+    };
+}
