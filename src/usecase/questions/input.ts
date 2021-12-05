a0a9397fc678222f36a32bb2f7271305d225d70e
matsushinDB11 <matsushin35@gmail.com>
2021-12-06 02:20:31 +0900

    create questions usecase

---
+++ src/usecase/questions/input.ts
@@ -0,0 +1,3 @@
+export type GetInput = {
+    questionID: number;
+};
---
+++ src/usecase/questions/logic.ts
@@ -0,0 +1,32 @@
+import { DBClient } from "../../domain/DBClient";
+import { question, Repository } from "../../domain/questions";
+import { Failure, Result, Success } from "../../errorHelper/resultType";
+import { GetInput } from "./input";
+import questions from "../../infra/repository/questions";
+
+const getList = async (
+    dbClient: DBClient,
+    questionsRepo: Repository
+): Promise<Result<question[], Error>> => {
+    const data = await questionsRepo.GetList(dbClient);
+    if (data.isFailure()) {
+        return new Failure(data.value);
+    } else {
+        return new Success(data.value);
+    }
+};
+
+const get = async (
+    dbClient: DBClient,
+    questionsRepo: Repository,
+    input: GetInput
+): Promise<Result<question, Error>> => {
+    const data = await questionsRepo.Get(dbClient, input.questionID);
+    if (data.isFailure()) {
+        return new Failure(data.value);
+    } else {
+        return new Success(data.value);
+    }
+};
+
+// export
