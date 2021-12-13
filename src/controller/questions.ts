fad6093a0f39b61713475800faef75f505f8a316
matsushinDB11 <matsushin35@gmail.com>
2021-12-13 23:41:16 +0900

    crate question get controller

--- src/controller/questions.ts
+++ src/controller/questions.ts
@@ -2,6 +2,7 @@ import { Interactor } from "../usecase/questions/endpoint";
 import { Request, Response } from "express";
 import ErrorToHttpStatus from "./errorToHttpStatus";
 import HttpStatusCodes from "../domain/httpStatusCodes";
+import { GetInput } from "../usecase/questions/input";
 const http = new HttpStatusCodes();
 
 class QuestionsController {
@@ -23,6 +24,24 @@ class QuestionsController {
             })
             .catch(() => {
                 return res.status(http.StatusInternalServerError()).send();
+            });
+    }
+    Get(req: Request, res: Response) {
+        const stringParam = req.params.id;
+        const input: GetInput = {
+            questionID: Number(stringParam),
+        };
+        this.interactor
+            .Get(input)
+            .then((data) => {
+                if (data.isFailure()) {
+                    res.status(ErrorToHttpStatus(data.value)).json(data.value);
+                } else {
+                    res.status(http.StatusOK()).json(data.value);
+                }
+            })
+            .catch(() => {
+                res.status(http.StatusInternalServerError()).send();
             });
     }
 }
